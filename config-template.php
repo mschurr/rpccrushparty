090af074308c3ae3c8760bece16bede288ba6fef
Matthew Schurr <mschurr@rice.edu>
2014-02-01 15:08:51 -0600

    More config, added htaccess for apache servers.

--- config-template.php
+++ config-template.php
@@ -2,9 +2,12 @@
 // Rename this file to config.php
 
 Config::set(array(
-	'database.driver' => 'mysql',
-	'database.user'   => 'httpd',
-	'database.pass'   => 'httpd',
-	'database.name'   => 'crushparty',
-	'crush.expired'   => false
+	'database.driver'   => 'mysql',
+	'database.host'     => 'localhost',
+	'database.port'     => '3306',
+	'database.user'     => 'httpd',
+	'database.pass'     => 'httpd',
+	'database.name'     => 'crushparty',
+	'crush.expired'     => false,
+	'cookies.secretKey' => ''
 ));
